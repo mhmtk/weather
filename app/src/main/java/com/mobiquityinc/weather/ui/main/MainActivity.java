0d633d0e24bca9bad1b90104f7695b440cb8f12d
Mehmet Kologlu <mkologlu@mobiquityinc.com>
2018-04-04 11:13:47 +0200

    Add fragment container to Main Activity

--- app/src/main/java/com/mobiquityinc/weather/ui/main/MainActivity.java
+++ app/src/main/java/com/mobiquityinc/weather/ui/main/MainActivity.java
@@ -1,9 +1,17 @@
-package com.mobiquityinc.weather;
+package com.mobiquityinc.weather.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.mobiquityinc.weather.R;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fragment_container_frame_layout)
+    protected FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
