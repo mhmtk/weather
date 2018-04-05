6bd6e36d31f69877d33bc629b6debaaf0be91002
Mehmet Kologlu <mkologlu@mobiquityinc.com>
2018-04-05 13:03:31 +0200

    Add tests to homescreen presenter

--- app/src/test/java/com/mobiquityinc/weather/HomeScreenPresenterTests.java
+++ app/src/test/java/com/mobiquityinc/weather/HomeScreenPresenterTests.java
@@ -4,6 +4,7 @@ package com.mobiquityinc.weather;
 import static org.mockito.Mockito.verify;
 
 import com.mobiquityinc.weather.domain.FavouriteCityRepository;
+import com.mobiquityinc.weather.domain.entities.City;
 import com.mobiquityinc.weather.ui.homescreen.HomeScreenContract;
 import com.mobiquityinc.weather.ui.homescreen.HomeScreenPresenter;
 
@@ -14,13 +15,17 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.IOException;
+
 @RunWith(MockitoJUnitRunner.class)
 public class HomeScreenPresenterTests {
 
     @Mock HomeScreenContract.View mockView;
     private HomeScreenPresenter presenter;
     @Mock
-    FavouriteCityRepository mMockFavouriteCityRepository;
+    FavouriteCityRepository mockFavouriteCityRepository;
+    @Mock
+    City city;
 
     @Before
     public void setup() {
@@ -29,22 +34,29 @@ public class HomeScreenPresenterTests {
 
     @Test
     public void createPresenter_setsView() {
-        presenter = new HomeScreenPresenter(mMockFavouriteCityRepository, mockView);
+        presenter = new HomeScreenPresenter(mockFavouriteCityRepository, mockView);
         verify(mockView).setPresenter(presenter);
     }
 
     @Test
     public void start_initiatesUI() {
-        presenter = new HomeScreenPresenter(mMockFavouriteCityRepository, mockView);
+        presenter = new HomeScreenPresenter(mockFavouriteCityRepository, mockView);
         presenter.start();
         verify(mockView).initiateUI();
     }
 
     @Test
     public void addCityButtonClicked_startsMapFragment() {
-        presenter = new HomeScreenPresenter(mMockFavouriteCityRepository, mockView);
+        presenter = new HomeScreenPresenter(mockFavouriteCityRepository, mockView);
         presenter.addCityButtonClicked();
         verify(mockView).startMapFragment();
+    }
+
+    @Test
+    public void onCityDeleteClicked_removesCity() throws IOException {
+        presenter = new HomeScreenPresenter(mockFavouriteCityRepository, mockView);
+        presenter.onCityDeleteClicked(city);
+        verify(mockFavouriteCityRepository).removeFavourite(city);
     }
 
 }
